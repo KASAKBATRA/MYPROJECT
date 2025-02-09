765f1ffe2e24789dec3f40d96ca345dd74b6ab8d
KASAK <140151041+KASAKBATRA@users.noreply.github.com>
2025-02-09 18:44:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,18 +82,19 @@ const enableBoxes = () => {
     }
 };
 
-const showWinner = () => {
-    msg.innertext = `congratulations the winner is ${winner}`;
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations! The winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
 };
 
 
+
 const checkWinner = () => {
     for(let pattern of winPatterns){
-        let pos1Val = boxes[pattern[0]].innertext;
-        let pos2Val = boxes[pattern[1]].innertext;
-        let pos3Val = boxes[pattern[1]].innertext;
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[1]].innerText;
 
         if (pos1Val != "" && pos2Val != "" && pos3Val != ""){
             if(pos1Val === pos2Val && pos2Val === pos3Val){
